b2bf94b8611a6dbf68a640470406019e1bf739ed
Kevin Zhang <kevin.zg98@gmail.com>
2018-07-29 21:50:35 -0400

    initial commit

---
+++ select-from-nobel.sql
@@ -0,0 +1,55 @@
+
+
+-- Question 1
+-- Change the query shown so that it displays Nobel prizes for 1950
+
+SELECT yr, subject, winner
+  FROM nobel
+WHERE yr = 1950;
+
+-- Question 2
+-- Show who won the 1962 prize for Literature
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature';
+
+-- Question 3
+-- Show the year and subject that won 'Albert Einstein' his prize
+
+SELECT yr,subject 
+  FROM nobel
+WHERE winner = 'Albert Einstein';
+
+-- Question 4
+-- Give the name of the 'Peace' winners since the year 2000, including 2000
+
+SELECT winner 
+  FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000;
+
+-- Question 5
+-- Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive
+
+SELECT yr,subject,winner FROM nobel
+WHERE yr >= 1980 AND yr <= 1989 AND subject = 'Literature'
+
+-- Question 6
+-- Show all details of the presidential winners:
+---* Theodore Roosevelt
+---* Woodrow Wilson
+---* Jimmy Carter
+---* Barack Obama
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter','Barack Obama');
+
+-- Question 7 
+-- Show the winners with first name John
+
+SELECT winner FROM nobel
+WHERE winner LIKE 'John%';
+
